e49bfbf511ee42d4c70fd575f7ce5a536318c197
luispadillareina <luispadillareina93@gmail.com>
2020-01-01 22:52:41 -0500

    Inicio

---
+++ EmapaiPrueba/EmapaiPrueba/Controllers/ClienteController.cs
@@ -0,0 +1,25 @@
+﻿using EmapaiPrueba.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace EmapaiPrueba.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET: Cliente
+        public ActionResult Index()
+        {
+            List<ClienteExterno> ListCliente;
+            using (DbConexion db = new DbConexion())
+            {
+                ListCliente = db.ClienteExternoes.Take(5).ToList();
+
+            }
+            return View(ListCliente);
+        }
+    }
+}
